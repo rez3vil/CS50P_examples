73420cc003ab8cf4a7ce0867b4acf764b073bfdf
rez3vil <125429015+rez3vil@users.noreply.github.com>
2023-07-24 16:25:39 +0530

    Update Lecture_1_Conditionals.py

--- Lecture_1_Conditionals.py
+++ Lecture_1_Conditionals.py
@@ -1,3 +1,5 @@
+#==================Calculator.py===========================
+
 #basic conditional code using if, elif and else
 x = int(input("What's x?"))
 y = int(input("What's y?"))
@@ -23,3 +25,31 @@ if x!=y:
     print("x is not equal to y")
 else:
     print ("x is equal to y")
+    
+#==================Grade.py===========================
+
+#using and as conditional
+score = int(input("Score: "))
+if score >= 90 and score <= 100:
+    print("Grade: A")
+elif score >= 80 and score <= 90:
+    print("Grade: B")
+elif score >= 70 and score <= 80:
+    print("Grade: C")
+elif score >= 60 and score <= 80:
+    print("Grade: D")
+else:
+    print("Grade: F")
+
+#simplifying above code
+score = int(input("Score: "))
+if 90 <= score <= 100:
+    print("Grade: A")
+elif 80 <= score <= 90:
+    print("Grade: B")
+elif 70 <= score <= 80:
+    print("Grade: C")
+elif 60 <= score <= 70:
+    print("Grade: D")
+else:
+    print("Grade: F")
