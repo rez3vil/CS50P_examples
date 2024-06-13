b0f560dbe96b4400ee1a204baa6e8b4d20a67fd8
rez3vil <125429015+rez3vil@users.noreply.github.com>
2024-06-13 17:37:20 +0530

    Create pset-3-2.py

---
+++ pset-3-2.py
@@ -0,0 +1,27 @@
+menu = {
+    "Baja Taco": 4.25,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
+    }
+
+def main():
+    total = 0.0
+    while True:
+        try:
+            item = input("Item: ").title()
+            if item in menu:
+                total += menu[item]
+                print(f"Total: ${format(total,'.2f')}")
+            else:
+                continue
+        except (KeyError, EOFError):
+            print(end="\n")
+            break
+
+main()
